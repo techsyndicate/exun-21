0abca14fa42517f6a5b1c90876aeafe436ca774b
gigabite-pro <rajmachawal19@gmail.com>
2022-01-16 21:22:39 +0530

    task form

--- admin/app.js
+++ admin/app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const indexRoute = require('./routes/indexRoute');
+const tasksRoute = require('./routes/tasks')
 const mongoose = require('mongoose')
 var db = process.env.db 
 
@@ -16,7 +17,8 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname+'public'));
 app.use(express.urlencoded({extended: true}));
 
-app.use('/',indexRoute)
+app.use('/admin', indexRoute)
+app.use('/tasks', tasksRoute)
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('Server started on port 3000');
